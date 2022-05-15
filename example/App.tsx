286f69ef2dc956d23ad18ea353262d213e76a944
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-16 01:24:10 +0600

    NPM Package builder

---
+++ example/App.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { Box } from "../src";
+const App = () => {
+	return <div>
+		<Box />
+	</div>
+}
+
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './hooks';
-export * from './library';
-export * from './components';
+import Box from './Box';
+
+export { Box };

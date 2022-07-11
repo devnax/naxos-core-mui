c5c091424cb52314ee3f093b092fb72d58459458
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-07-12 00:06:57 +0600

    System finish

--- src/System/DefaultVars.ts
+++ src/System/DefaultVars.ts
@@ -1,41 +1,8 @@
 import { SystemProps } from './types/SystemVars';
 
-const Background = {
-    backgroundColor: '',
-    backgroundImage: '',
-    blur: 0
-};
 
 const defaultValues: SystemProps = {
-    Desktop: {
-        pinnedWidgets: [],
-        background: Background,
-        grids: [],
-        pinnedApps: [],
-        sortBy: 'name'
-    },
-    App: {
-        runAppId: 'desktop'
-    },
-    Screen: {
-        background: Background
-    },
-    Dock: {},
-    Settings: {
-        background: Background
-    },
-    WidgetDrawer: {
-        background: Background
-    },
-    Themes: {},
-    KeyBind: {},
-
-    // Lock and Login
-    LockScreen: {
-        background: Background,
-        showDate: true,
-        pinnedApps: []
-    }
+    RunnedApp: null
 };
 
 export default defaultValues;
--- src/System/types/SystemVars.ts
+++ src/System/types/SystemVars.ts
@@ -1,43 +1,4 @@
-import { ReactElement, SVGProps } from 'react';
-import { ThemeOptions } from '@mui/material/styles';
-import { DockProps } from '../../Dock/types'
-type Background = {
-    backgroundColor: string;
-    backgroundImage: string;
-    blur: number;
-};
 
 export interface SystemProps {
-    Desktop: {
-        pinnedApps: string[];
-        pinnedWidgets: string[];
-        background: Background;
-        grids: string[];
-        sortBy: 'name' | 'date' | 'title';
-    };
-    App: {
-        runAppId: string;
-    };
-    Screen: {
-        background: Background;
-    };
-    Dock: DockProps;
-    Settings: {
-        background: Background;
-    };
-    WidgetDrawer: {
-        background: Background;
-    };
-    Themes: ThemeOptions;
-
-    KeyBind: {
-        [key: string]: string; // value = CMD Key
-    };
-
-    // Lock and Login
-    LockScreen: {
-        background: Background;
-        showDate: Boolean;
-        pinnedApps: string[];
-    };
+    RunnedApp: string | null;
 }
